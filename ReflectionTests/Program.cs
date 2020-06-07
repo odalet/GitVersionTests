768e8cfb448039b2a6e07492f7c0a8cb4aa81353
odalet <odalet@gmail.com>
2020-06-07 18:04:14 +0200

    more tests

---
+++ ReflectionTests/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ReflectionTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test(typeof(ClassLibrary1.Class1).Assembly);
+            Test(typeof(ClassLibrary2.Class1).Assembly);
+            Test(typeof(ClassLibrary3.Class1).Assembly);
+        }
+
+        private static void Test(Assembly assembly)
+        {
+            Console.WriteLine("------------------------------------------------------------------------");
+            var assemblyName = assembly.GetName().Name;
+            Console.WriteLine($"Testing {assemblyName}");
+            Console.WriteLine("All Fields:");
+            ////var gitVersionInformationType = assembly.GetType(assemblyName + ".GitVersionInformation");
+            var gitVersionInformationType = assembly.GetType("GitVersionInformation");
+            var fields = gitVersionInformationType.GetFields();
+            var properties = gitVersionInformationType.GetProperties();
+            foreach (var field in fields)
+                Console.WriteLine(string.Format("{0}: {1}", field.Name, field.GetValue(null)));
+            foreach (var property in properties)
+                Console.WriteLine(string.Format("{0}: {1}", property.Name, property.GetGetMethod(true).Invoke(null, null)));
+
+            Console.WriteLine();            
+            Console.WriteLine("Specific Field:");
+            var versionField = gitVersionInformationType.GetField("Major");
+            if (versionField != null)
+                Console.WriteLine(versionField.GetValue(null));
+            else
+            {
+                var versionProperty = gitVersionInformationType.GetProperty("Major");
+                if (versionProperty != null)
+                    Console.WriteLine(versionProperty.GetGetMethod(true).Invoke(null, null));
+            }
+        }
+    }
+}
---
+++ ReflectionTestsFS/Program.fs
@@ -0,0 +1,39 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Reflection
+
+let test (assembly : Assembly) =
+    printfn "------------------------------------------------------------------------"
+    let assemblyName = assembly.GetName().Name
+    printfn "Testing %s" assemblyName
+
+    let gitVersionInformationType = assembly.GetType "GitVersionInformation"
+    let fields = gitVersionInformationType.GetFields()
+    let properties = gitVersionInformationType.GetProperties()
+
+    for field in fields do
+        printfn "%s: %s" field.Name (field.GetValue(null) :?> string)
+        
+    for property in properties do
+        printfn "%s: %s" property.Name (property.GetGetMethod(true).Invoke(null, null) :?> string)
+
+    printfn ""
+    printfn "Specific Field:"
+    let versionField = gitVersionInformationType.GetField "Major"
+    if not (isNull versionField)
+    then printfn "%s" (versionField.GetValue(null) :?> string)
+    else
+        let versionProperty = gitVersionInformationType.GetProperty "Major"
+        if not (isNull versionProperty)
+        then printfn "%s" (versionProperty.GetGetMethod(true).Invoke(null, null) :?> string)
+    ()
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    test typeof<ClassLibrary1.Class1>.Assembly
+    test typeof<ClassLibrary2.Class1>.Assembly
+    test typeof<ClassLibrary3.Class1>.Assembly
+    0 // return an integer exit code
+
