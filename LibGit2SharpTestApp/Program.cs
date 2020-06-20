4b4e224b8446f8dc50ce8162bb252f43d71c45be
odalet <odalet@gmail.com>
2020-06-20 14:43:29 +0200

    Playing with LibGit2Sharp

---
+++ LibGit2SharpTestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LibGit2SharpTestLib;
+
+namespace LibGit2SharpTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Test().Run();
+            Console.WriteLine($"Found: {result}");
+        }
+    }
+}
---
+++ LibGit2SharpTestLib/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using LibGit2Sharp;
+
+namespace LibGit2SharpTestLib
+{
+    public class Test
+    {
+        public string Run()
+        {
+            var dotgit = GetDotgitDirectory();
+            if (dotgit == null) return null;
+
+            var result = Repository.Discover(dotgit);
+            return result;
+        }
+
+        private string GetDotgitDirectory()
+        {
+            var directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+            //var directory = new DirectoryInfo(@"C:\Users\odalet\AppData\Local\Temp\TestRepositories\0c7af7b1-6f7f-4854-87e0-2a3ed9a03059");
+            while (true)
+            {
+                if (directory == null)
+                    return null;
+
+                if (directory.EnumerateDirectories(".git", SearchOption.TopDirectoryOnly).Any())
+                    return directory.ToString();
+
+                directory = directory.Parent;
+            }
+        }
+    }
+}
